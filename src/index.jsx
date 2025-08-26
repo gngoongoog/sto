2b386020afff40d373fdc3f33f2c45f41e7918ca
gngoongoog <95882142+gngoongoog@users.noreply.github.com>
2025-08-26 22:46:37 +0500

    index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./style.css";
 
-import { App } from './App.jsx'
-
-ReactDOM.createRoot( 
-  document.querySelector('#root')
-).render(<App />)
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
